da5170041c4c526c56f7cbd490f4b53fd6ab0885
Tong <1427275295@qq.com>
2025-05-13 01:55:56 +0800

    config preliminary router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './views/Login';
+import Layout from './views/Layout';
 
 function App() {
   return (
-    <div className="App">
-      App根组件
-    </div>
+    <Router>
+      <div>
+        {/* <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/layout">Layout</Link>
+          </li>
+        </ul>
+        <hr /> */}
+
+        <Routes>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/layout" element={<Layout/>}></Route>
+          <Route path="/">
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
